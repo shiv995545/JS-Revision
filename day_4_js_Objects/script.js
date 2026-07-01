1d5c341474a013f14a586e6bb4f4db242f449339
shiv995545 <shvgpt2001@gmail.com>
2026-07-01 17:08:24 +0530

    Objects Basics Complete

--- day_4_js_Objects/script.js
+++ day_4_js_Objects/script.js
@@ -1,7 +1,7 @@
 //=============================OBJECTS===============================
 
 // Definition = A standalone, dynamic data structure used to store collections of related data and complex entities as key-value pairs
-// -------it stores named data
+// --------it stores named data
 
 
 
@@ -86,7 +86,7 @@ let user1 = {
 
 
 
-// 6. Object Destructured ===== 
+// 6. Object Destructuring ===== 
 
 
 // Extract properties into variables, cleanly---------
@@ -138,9 +138,107 @@ let updated = {...person, age : 80}
 
 
 
-//Methods in Object --------------------------------
+// 8. Methods in Object --------------------------------
 
 // console.log(Object.keys(person))
 // console.log(Object.values(person))
 // console.log(Object.entries(person))
+
+// Object.entries(person).forEach(([key, value]) => {
+//     console.log(key + value)                                      //accessing key value pairs using forEach(iteration)
+// })
+
+
+
+
+
+
+
+//++++++++++++++++++++shallow and deep copy in objects+++++++++++++++++++
+
+
+//we can use spread and assign to make shallow copy
+let formed = Object.assign({}, user1)             //shallow copy
+
+// formed.name = "Shivam"                       
+// formed.address.city = "Mumbai"
+
+// console.log(user1)                           //original
+// console.log(formed)                          //copied
+
+
+
+let formed1 = structuredClone(user1)                //deep copy
+
+formed1.name = "Shivam"
+formed1.address.city = "Mumbai"
+
+// console.log(formed1)                     //copied
+// console.log(user1)                      //original
+
+
+
+
+
+
+
+//==========Object.freeze & Object.seal=====================
+
+// Freeze ------- Prevents the modification of existing property attributes and values, and prevents the addition of new properties.
+let animal = {
+    name : "Dog",
+    age : 20,
+    breed : "Pet"
+}
+Object.freeze(animal)
+
+animal.name = "Lion"
+animal.mood = "Happy"
+// console.log(animal)
+
+
+
+// Seal ------ Prevents the modification of attributes of existing properties, and prevents the addition of new properties.
+let animal2 = {
+    name : "Dog",
+    age : 20,
+    breed : "Pet"
+}
+Object.seal(animal2)
+
+animal2.name = "Lion"
+animal2.mood = "Happy"
+// console.log(animal2)
+
+
+
+
+
+
+// 9. ========================LOOPS THROUGH OBJECTS==================
+
+
+let color = {
+    name : "Pink",
+    kind : "Pastel",
+    count : 10
+}
+
+for(let key in color){
+    console.log(key, ":", color[key]);
+}
+
+Object.keys(color).forEach(key => {
+    console.log(key, ":",  color[key]);
+})
+
+
+Object.entries(person).forEach(([key, value]) => {
+    console.log(key + value)                                      //accessing key value pairs using forEach(iteration)
+})
+
+
+for(let [key, value] of Object.entries(color)) {
+    console.log(key, ":", value)
+}
 
